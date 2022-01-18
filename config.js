a37b7f8055dedd268d2e3e08e6f0171c591211ad
Ranjan <skillsapphire@gmail.com>
2022-01-18 12:27:01 +0100

    changes

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URI: "mongodb+srv://myappuser:0KzcHe7OpRuWjJaT@cluster0.mooo2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    MONGODB_URI: "UR BD URL",
     JWT_SECRET: "ahfgjshfgsjedgfs32654784eefbedfew"
 }
